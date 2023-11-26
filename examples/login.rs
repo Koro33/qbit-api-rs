449028792e86d500630583737281efffea053a67
Koro33 <koro33g@gmail.com>
2023-11-26 22:18:21 +0100

    feat: :sparkles: update example

--- examples/login.rs
+++ examples/login.rs
@@ -3,28 +3,25 @@ use qbit_api_rs::client::QbitClient;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // setup logger
+    //// setup logger
     std::env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    // initialize client with given username and password
-    let qbit =
+    //// initialize client with given username and password
+    let qb_cli =
         QbitClient::new_with_user_pwd("http://192.168.0.11:8080", "admin", "adminadmin").unwrap();
 
-    // or from env(QBIT_HOST, QBIT_USERNAME, QBIT_PASSWORD)
-    // let qbit = QbitClient::new_from_env().unwrap();
+    //// or from env(QBIT_HOST, QBIT_USERNAME, QBIT_PASSWORD)
+    // let qb_cli = QbitClient::new_from_env().unwrap();
 
-    // login
-    qbit.auth_login().await?;
+    //// login
+    qb_cli.auth_login().await?;
 
-    let v = qbit.app_version().await?;
-    log::info!("app version: {:?}", v);
+    log::info!("app version: {:?}", qb_cli.app_version().await?);
 
-    let buildinfo = qbit.app_build_info().await?;
-    log::info!("buildinfo: {:?}", buildinfo);
+    log::info!("buildinfo: {:?}", qb_cli.app_build_info().await?);
 
-    let wv = qbit.app_webapi_version().await?;
-    log::info!("webapi_version: {:?}", wv);
+    log::info!("webapi_version: {:?}", qb_cli.app_webapi_version().await?);
 
     Ok(())
 }
